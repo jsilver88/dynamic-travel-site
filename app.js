f7d1ce9dd560654deb3a6ba02affae10ff5db6d8
Jared Silver <jrsilver88@gmail.com>
2020-12-24 11:06:01 -0800

    added cursor circle and navbar functionality

--- app.js
+++ app.js
@@ -57,5 +57,52 @@ function animateSlides() {
       .addTo(controller);
   });
 }
+const mouse = document.querySelector('.cursor');
+const mouseText = mouse.querySelector('span');
+const burger = document.querySelector('.burger');
+function cursor(e) {
+  mouse.style.top = e.pageY + 'px';
+  mouse.style.left = e.pageX + 'px';
+}
+
+function activeCursor(e) {
+  const item = e.target;
+  if (item.id === 'logo' || item.classList.contains('burger')) {
+    mouse.classList.add('nav-active');
+  } else {
+    mouse.classList.remove('nav-active');
+  }
+  if (item.classList.contains('explore')) {
+    mouse.classList.add('explore-active');
+    gsap.to('.title-swipe', 1, { y: '0%' });
+    mouseText.innerText = 'Tap';
+  } else {
+    mouse.classList.remove('explore-active');
+    mouseText.innerText = '';
+    gsap.to('.title-swipe', 1, { y: '110%' });
+  }
+}
+
+function navToggle(e) {
+  if (!e.target.classList.contains('active')) {
+    e.target.classList.add('active');
+    gsap.to('.line1', 0.5, { rotate: '45', y: 5, background: 'black' });
+    gsap.to('.line2', 0.5, { rotate: '-45', y: -5, background: 'black' });
+    gsap.to('#logo', 1, { color: 'black' });
+    gsap.to('.navbar', 1, { clipPath: 'circle(2500px at 100% -10%)' });
+    document.body.classList.add('hide');
+  } else {
+    e.target.classList.remove('active');
+    gsap.to('.line1', 0.5, { rotate: '0', y: 0, background: 'white' });
+    gsap.to('.line2', 0.5, { rotate: '0', y: 0, background: 'white' });
+    gsap.to('#logo', 1, { color: 'white' });
+    gsap.to('.navbar', 1, { clipPath: 'circle(50px at 100% -10%)' });
+    document.body.classList.remove('hide');
+  }
+}
+
+burger.addEventListener('click', navToggle);
+window.addEventListener('mousemove', cursor);
+window.addEventListener('mouseover', activeCursor);
 
 animateSlides();
